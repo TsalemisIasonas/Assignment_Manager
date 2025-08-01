d0f289e4297fa2cfeb798b4b6d7cc53057adbbae
TsalemisIasonas <tsalemisi@gmail.com>
2025-08-01 13:42:05 +0300

    Colors as Constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color lightGreen = const Color.fromARGB(255, 12, 113, 105);
+Color darkGreen = const Color.fromARGB(255, 4, 81, 63);
+Color navbarColor = const Color.fromARGB(255, 4, 71, 55);
+Color navbarIconColor = const Color.fromARGB(255, 210, 201, 201);
+Color backgroundColor = const Color.fromARGB(255, 0, 0, 0);
+Color textColor = const Color.fromARGB(255, 255, 255, 255);
